3507002d9856e1052cfc3db0dc78cc56a987e5fe
Ahmed Elsharif <elsharif@gmail.com>
2017-08-15 01:27:31 -0500

    updating constants

--- zillowlib/ZillowConstants.cs
+++ zillowlib/ZillowConstants.cs
@@ -3,7 +3,7 @@ namespace zillowlib
 {
     public static class ZillowConstants
     {
-        public const string zwid = "X1-ZWz1fxu4dh7dhn_1sm5d";
+        public const string zwid = "xxxx";
         public const string SearchPropertyUrl = "http://www.zillow.com/webservice/GetSearchResults.htm";
         public const string GetPricingUrl = "http://www.zillow.com/webservice/GetZestimate.htm";
     }
